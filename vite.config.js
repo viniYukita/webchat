bc41e6ef22d381d18922f6a648647de21a061597
yuki <yukita.vinicius@gmail.com>
2024-07-30 11:38:43 -0300

    Alterado porta utilizada

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '172.20.31.35',
+    port: 5173,
+  }
 })
