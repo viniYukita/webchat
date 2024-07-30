4172ff9e891aef46a3c36b2934d56631e7d08ef4
yuki <yukita.vinicius@gmail.com>
2024-07-30 11:43:19 -0300

    Alterado host

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '172.20.31.35',
+    host: 'srvmaklen-chat',
     port: 5173,
   }
 })
